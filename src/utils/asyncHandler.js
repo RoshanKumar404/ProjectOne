57c5e8abe6ebf95568047e85af45272a78a28513
RoshanKumar404 <roshankuar951@gmail.com>
2024-10-20 23:31:20 +0530

    h

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,13 +1,25 @@
-const asyncHandler=(fun)=>async(req,res,next)=>{
-
-    try {
-         await fun(req,res,next)
-    } catch (error) {
-        error.status(error.code || 400 ).json({
-            success:false,
-            message:error.message
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>{
+            next(err)
         })
     }
 }
 
+// ***************this is the  try and catch method*************
+
+// const asyncHandler=(fun)=>async(req,res,next)=>{
+
+//     try {
+//          await fun(req,res,next)
+//     } catch (error) {
+//         error.status(error.code || 400 ).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
+
 export {asyncHandler}
+
+
